b8c0e38f6d12f73cac7acfa342640270614cc7f7
Yordan Krushkov <yordankrushkov@gmail.com>
2021-01-09 16:20:36 +0000

    feat/ connected to DB

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const env = process.env.NODE_ENV || 'development';
-
 const config = require('./config/config')[env];
 const app = require('express')();
 const router = require('./Router/router')
-
+const mongoose=require('mongoose');
 require('./config/express')(app);
 app.use('/', router)
+mongoose.connect(config.dbUrl,{ useNewUrlParser: true,useUnifiedTopology: true } , err=>{
+    if(err){
+        console.log('Error:', err);
+        return;
+    }
 
+console.log('Connected to the db');
+})
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+
+
