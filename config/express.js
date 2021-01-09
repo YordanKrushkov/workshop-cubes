4061f5f4f08dc40a264fa6fdc03d408ccaeb263c
Yordan Krushkov <yordankrushkov@gmail.com>
2021-01-09 12:18:26 +0000

    feat/refactoring

---
+++ config/express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+
+module.exports = (app) => {
+    app.use(express.json());
+    app.use(express.urlencoded());
+    app.engine('.hbs', handlebars({
+        extname: '.hbs'
+    }));
+    app.set('view engine', '.hbs');
+    app.use(express.static('static'));
+};
